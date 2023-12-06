e51715eba1ab8e32550989e41385fc372b3feba4
JatinMeh <jatin3105@gmail.com>
2023-12-06 18:35:36 +0530

    Removed FetchID

--- main.js
+++ main.js
@@ -1,36 +1,34 @@
 function redirectToMovie() {
-    const movieId = document.getElementById('movieIdInput').value;
+    const movieTitle = document.getElementById('movieIdInput').value;
 
-    const movieUrlTemplate = 'https://vidsrc.to/embed/movie/{id}';
-    const movieUrl = movieUrlTemplate.replace('{id}', movieId);
-
-    window.open(movieUrl, '_blank');
+    fetchIMDbID(movieTitle, 'movie');
 }
 
 function redirectToTv() {
-    const tvId = document.getElementById('tvIdInput').value;
+    const movieTitle = document.getElementById('tvIdInput').value;
 
-    const tvUrlTemplate = 'https://vidsrc.to/embed/tv/{id}';
-    const tvUrl = tvUrlTemplate.replace('{id}', tvId);
-
-    window.open(tvUrl, '_blank');
+    fetchIMDbID(movieTitle, 'tv');
 }
 
-const omdbkey='44991e20'
+const omdbkey = '44991e20';
 
-function fetchIMDbID() {
+function fetchIMDbID(title, type) {
     const apiKey = omdbkey;
-    const movieTitle = document.getElementById('movieTitle').value;
-    const apiUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(movieTitle)}&apikey=${apiKey}`;
+    const apiUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(title)}&apikey=${apiKey}`;
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             if (data.Response === 'True') {
+                document.getElementById('result').innerHTML = 'Media found.';
                 const imdbID = data.imdbID;
-                document.getElementById('result').innerHTML = `${imdbID}`;
+                if (type === 'movie') {
+                    redirectToMovieById(imdbID);
+                } else {
+                    redirectToTvById(imdbID);
+                }
             } else {
-                document.getElementById('result').innerHTML = 'Movie not found. Please check the title.';
+                document.getElementById('result').innerHTML = 'Media not found. Please check the title.';
             }
         })
         .catch(error => {
@@ -39,5 +37,16 @@ function fetchIMDbID() {
         });
 }
 
+function redirectToMovieById(movieId) {
+    const movieUrlTemplate = 'https://vidsrc.to/embed/movie/{id}';
+    const movieUrl = movieUrlTemplate.replace('{id}', movieId);
 
+    window.open(movieUrl, '_blank');
+}
 
+function redirectToTvById(tvId) {
+    const tvUrlTemplate = 'https://vidsrc.to/embed/tv/{id}';
+    const tvUrl = tvUrlTemplate.replace('{id}', tvId);
+
+    window.open(tvUrl, '_blank');
+}
